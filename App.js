34bc39ad1a13be4ab6e73b1ba7e750dca280083f
nitturunithin4444 <nitturunithin4444@gmail.com>
2024-07-05 20:41:04 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,27 +1,16 @@
 //import logo from './logo.svg';
 //import './App.css';
 import React from 'react'
-import Form from './login';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import Layout from './login';
-import Registration from './login';
-import {useEffect} from 'react';
+import Add from './first';
 function App() {
   return (
     <div>
-      <>
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout/>}></Route>
-        <Route index element={<Registration/>}></Route>
-        <Route path="login" element={<login/>}></Route>
-        <Route path=" " elment={<sign up/>}></Route>
-        </Routes>
-        </BrowserRouter>
-        <useEffect/>
-        </>
+      <Add>
+
+      </Add>
     </div>
   );
 }
 
 export default App;
+ 
