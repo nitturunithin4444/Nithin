eaa8e32114cabf775d5dce1ed0cb28edf1c616ae
nitturunithin4444 <nitturunithin4444@gmail.com>
2024-07-06 13:14:55 +0530

    Add files via upload

--- pathmodule.js
+++ pathmodule.js
@@ -2,13 +2,13 @@ const {fileURLToPath}=require('url');
 const path=require('path');
 const {readFile}=require('fs');
 const {writeFile}=require('fs');
-const _filename=fileURLToPath(import.meta.url);
-const __dirname=path.dirname(_filename);
-console.log(_dirname);
-readFile(path.join(__dirname),'utf-8',(err,data)=>{
+const _filename=__filename;
+const __dirname1=path.dirname(_filename);
+console.log(__dirname1);
+readFile(path.join(__dirname1,'customers.json'),'utf-8',(err,data)=>{
     if(err) throw err;
     let customers=JSON.parse(data);
-    writeFile(path.join(_dirname,'util','customers.join'),JSON.stringify(customers),'utf-8',(err,data)=>{
+    writeFile(path.join(__dirname1,'customers.json'),JSON.stringify(customers),'utf-8',(err,data)=>{
         if(err) throw err;
         console.log("Data written successfully")
     })
